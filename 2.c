daef9c501cd8bc286aa1b43fbcccdb7ce716c734
gggxbbb <2331490629@qq.com>
2023-07-17 13:58:54 +0800

    2

---
+++ 2.c
@@ -0,0 +1,11 @@
+//
+// Created by gggxbbb on 2023/7/17.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    unsigned long long test[2048];
+    getchar();
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,15 @@
+//
+// Created by gggxbbb on 2023/7/17.
+//
+
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<unsigned long long> test;
+    for (;;) {
+        test.push_back(114514);
+    }
+    return 0;
+}
